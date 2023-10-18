4e2b53a25badb1b7218068f1e63ea99966ca936d
Raylan <raylanzeeroo@outlook.com>
2023-10-18 16:50:17 -0300

    User update endpoint

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -16,11 +16,39 @@ class UserController extends Controller
         return UserResource::collection($users); // -> Retorna os dados que eu quero dentro de um data
     }
 
+    public function showOneUser(string $id)
+    {
+        //1ª forma:
+        // $user =  User::find($id);
+
+        //2ª forma:
+        // $user = User::where('id', '=', $id)->first();
+        // if (!$user) {
+        //     return response()->json(['message' => 'USER NOT FOUND'], 404); // -> manda uma mensagem manualmente informando que o ususário não foi encontrado.
+        // } else {
+        //     return new UserResource($user);
+        // }
+
+        //3ª forma:
+        $user = User::findOrFail($id); // -> Pega o usuário que veio pelo get ou já manda um response caso não exista.
+        return new UserResource($user);
+    }
+
     public function store(StoreUpdateUserRequest $request) // -> injeção de dependência
     {
         $data = $request->validated(); // -> Nesse caso posso usar esse método pra pegar apenas os dados validados na requisição
         $data['password'] = bcrypt($request->password); // -> criptografando a senha
         $user = User::create($data); // -> Estou salvando esses dados na tabela usuário
         return new UserResource($user); // -> retorna o usuário cadastrado no formato que foi definido no UserResource
+    }
+
+    public function update(Request $request, string $id) // -> aqui estou recebendo tanto o id do usuário que será modificado, como as modificações.
+    {
+        $data = $request->all();
+        $data['password'] = bcrypt($request->getPassword());
+        $user = User::findOrFail($id);
+        $user->update($data); // -> Estando tudo certo com $user a requição faz atualização dos dados.
+
+        return new UserResource($user);
     }
 }
